478974b920976d173a905c71f55ea20aa17f2103
Vince <github@darkain.com>
2016-08-05 09:21:25 -0700

    Added a series of unit tests for magnets

--- test/magnet.php
+++ test/magnet.php
@@ -1,6 +1,198 @@
 <?php
 
 
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>'']);
+
+tbxTest('ab');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>NULL]);
+
+tbxTest('ab');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>0]);
+
+tbxTest('a<elem>0</elem>b');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>1]);
+
+tbxTest('a<elem>1</elem>b');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>'1']);
+
+tbxTest('a<elem>1</elem>b');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>true]);
+
+tbxTest('a<elem>1</elem>b');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>false]);
+
+tbxTest('ab');
+
+
+
+
+$tbx->loadString('a<elem>[x.val;magnet=elem]</elem>b')
+	->field('x', ['val'=>0.0]);
+
+tbxTest('a<elem>0</elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>'']);
+
+tbxTest('a<elem></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>NULL]);
+
+tbxTest('a<elem></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>0]);
+
+tbxTest('a<elem prop="0"></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>1]);
+
+tbxTest('a<elem prop="1"></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>'1']);
+
+tbxTest('a<elem prop="1"></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>true]);
+
+tbxTest('a<elem prop="1"></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>false]);
+
+tbxTest('a<elem></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"></elem>b')
+	->field('x', ['val'=>0.0]);
+
+tbxTest('a<elem prop="0"></elem>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>'']);
+
+tbxTest('a<elem/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>NULL]);
+
+tbxTest('a<elem/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>0]);
+
+tbxTest('a<elem prop="0"/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>1]);
+
+tbxTest('a<elem prop="1"/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>'1']);
+
+tbxTest('a<elem prop="1"/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>true]);
+
+tbxTest('a<elem prop="1"/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>false]);
+
+tbxTest('a<elem/>b');
+
+
+
+
+$tbx->loadString('a<elem prop="[x.val;magnet=#]"/>b')
+	->field('x', ['val'=>0.0]);
+
+tbxTest('a<elem prop="0"/>b');
+
+
+
+
 $tbx->loadString('<elem prop="[x.prop;magnet=#;noerr]" attr="[x.attr;block=elem]">test</elem>')
 	->block('x', [['attr'=>1], ['attr'=>2]]);
 
