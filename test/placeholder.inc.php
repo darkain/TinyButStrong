e1fcb856c9c042c270033fe8822a7b7938c04c80
Vince <git@darkain.com>
2018-06-18 09:50:32 -0700

    Adding more unit tests for placeholder

--- test/placeholder.inc.php
+++ test/placeholder.inc.php
@@ -45,3 +45,35 @@ $tbx->loadString("a[x.item;placeholder='1']b")
 	->field('x', ['item' => 1]);
 
 tbxTest('ab');
+
+
+
+
+$tbx->loadString("a[x.item;placeholder='0']b")
+	->field('x', ['item' => (float)0.0]);
+
+tbxTest('ab');
+
+
+
+
+$tbx->loadString("a[x.item;placeholder='0']b")
+	->field('x', ['item' => (float)1.0]);
+
+tbxTest('a1b');
+
+
+
+
+$tbx->loadString("a[x.item;placeholder='1']b")
+	->field('x', ['item' => (float)0.0]);
+
+tbxTest('a0b');
+
+
+
+
+$tbx->loadString("a[x.item;placeholder='1']b")
+	->field('x', ['item' => (float)1.0]);
+
+tbxTest('ab');
